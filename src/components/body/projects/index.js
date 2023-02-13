971676420639555809b064e7a2c24daf1ec9fbf8
Dheerendra Kumar <53529978+have01@users.noreply.github.com>
2023-02-13 17:33:29 +0530

    Update index.js

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -7,17 +7,19 @@ import "./projects.css";
 function Projects() {
   const data = ProjectData;
   return (
-    <AnimationOnScroll animateIn="animate__fadeInRight">
+   
       <div className="projects">
         <Separator />
         <label className="section-title">Projects</label>
+     <AnimationOnScroll animateIn="animate__fadeInRight">
         <div>
           {data.map((project) => {
             return <ProjectCard project={project} />;
           })}
         </div>
-      </div>
     </AnimationOnScroll>
+      </div>
+
   );
 }
 
